7b40df88724ecb36f1b290cdbc74ed68983a183a
hazminfirdaus <cmhazminfirdaus@gmail.com>
2020-06-26 17:42:52 +0800

    fetch flights

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react';
-
+import { DateTime } from 'luxon';
 import './App.css';
 
+const url = 'https://api.skypicker.com/flights?flyFrom=PRG&to=VLC&dateFrom=27/06/2020&dateTo=27/07/2020&partner=picky&v=3&offset=0&limit=5'
 
-const url = 'https://api.skypicker.com/flights?flyFrom=PRG&to=LGW&dateFrom=18/11/2020&dateTo=12/12/2020&partner=picky&v=3'
+
 const App = () => {
   const [ flights, setFlights ] = useState([]);
 
@@ -18,15 +19,23 @@ const App = () => {
     fetchData();
   }, []);
 
+
+
   return(
     <div className="App">
     
     <h2>Flights</h2>
     {flights.map((flight, index) => (
       <div key={index}>
-        <p>{flight.flyFrom}</p>
-        <p>{flight.flyTo}</p>
-      </div>))};
+        <p>Price: {flight.price} EUR</p>
+        <h2>{flight.cityFrom}</h2>
+        <p>From: {flight.flyFrom}</p>
+        <h2>{flight.cityTo}</h2>
+        <p>To: {flight.flyTo}</p>
+        <p>Departure: {DateTime.fromMillis(flight.dTime * 1000).toFormat('hh:mm')}</p>
+        <p>Arrival: {DateTime.fromMillis(flight.aTime * 1000).toFormat('hh:mm')}</p>
+        <br/><hr/>
+      </div>))}
     
     </div>
   )
